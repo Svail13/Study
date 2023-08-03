bbeae0bc4f45fc48c30eedd51c715628025379b8
Kanstantsin Kuzmin <svail.13@gmail.com>
2023-08-03 11:30:53 +0300

    complete 1

--- practice_1.py
+++ practice_1.py
@@ -1,4 +1,17 @@
+#Практическое занятие 1: Простые операции с переменными
+'''
+x = int;
+y = int;
 x=10;
 y=5;
+
 summa = x + y;
-print("summa =",summa);
+minus = x - y; 
+umnojenie = x * y;
+delenie = x / y;
+print("summa =", summa, "minus =", minus, "umnojenie =", umnojenie, "delenie =", delenie);
+'''
+
+
+#Практическое занятие 2: Работа со строками
+
